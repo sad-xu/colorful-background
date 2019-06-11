735a00cc4f99671296e44599f44efe42de5a2429
xhc <1031568754@qq.com>
2019-06-11 23:47:25 +0800

    feat: add some configuration properties

--- src/colorfulBackground.js
+++ src/colorfulBackground.js
@@ -8,10 +8,8 @@ const vscode = require('vscode')
  * 安装：
  *  替换css源文件
  * 
- * 
  * 卸载：
  *  剔除新增代码
- * 
  */
 const CSS_PATH = path.join(path.dirname(require.main.filename), 'vs', 'workbench', 'workbench.main.css')
 
@@ -21,14 +19,53 @@ function clearCssContent(cssContent) {
   } else return cssContent
 }
 
+/**
+ * 自定义配置
+ *  animate 静态/动态
+ *  方向 top/right/bottom/left
+ *  colors 
+ *  animation-duration
+ *  animation-timing-function
+ * 
+ */
+const COLOR_MAP = {
+  'DeepSpace': '#000000, #434343',
+  'ServQuick': '#485563, #29323c',
+  'MidnightCity': '#232526, #414345',
+  'Anwar': '#334d50, #cbcaa5',
+  'NoontoDusk': '#ff6e7f, #bfe9ff'
+}
+
+function generateStyle({
+  animate = true,
+  duration = 15,
+  timingFunction = 'ease',
+  rotate = -45,
+  mode = ''
+  // colors
+}) {
+  let style = ''
+  let colors = ''
+  if (COLOR_MAP[mode]) colors = COLOR_MAP[mode]
+  style += `background: linear-gradient(${rotate}deg, ${colors});`
+  if (animate) {
+    style += `background-size: 400% 400%;`
+      + `animation: xhc-diy-animation ${duration}s ${timingFunction} infinite;`
+  }
+  return style
+}
+
+
 function init() {
-  // const CONFIG = vscode.workspace.getConfiguration('colorfulBackground')
-  // console.log(CONFIG)
+  const CONFIG = vscode.workspace.getConfiguration('colorfulBackground')
+  console.log(CONFIG)
+
+
   let cssContent = fs.readFileSync(CSS_PATH, 'utf-8')
   // 1. 清除历史样式
   cssContent = clearCssContent(cssContent)
   // 2. 生成新样式
-  let newCss = 'background: linear-gradient(-45deg,#ee7752,#e73c7e,#23a6d5,#23d5ab);background-size: 400% 400%;animation: xhc-diy-animation 15s ease infinite;'
+  let newCss = generateStyle(CONFIG) // 'background: linear-gradient(-45deg,#ee7752,#e73c7e,#23a6d5,#23d5ab);background-size: 400% 400%;animation: xhc-diy-animation 15s ease infinite;'
   // 3. 添加新样式
   cssContent += `/*colorful-background-start*/`
   + `[id="workbench.parts.editor"] .split-view-view .editor-container .overflow-guard>.monaco-scrollable-element>.monaco-editor-background{${newCss}}`
